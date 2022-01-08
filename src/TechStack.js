6a4e5e1f5b844d2ccfddb1f0dcbf888b633cd384
Keen Wilson <kwilson@finsignum.com>
2022-01-07 18:50:46 -0800

    Started teach stack

--- src/TechStack.js
+++ src/TechStack.js
@@ -5,15 +5,14 @@ function TechStack({ title }) {
     <div className="py-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+          <h2 className="text-base text-stone-600 font-semibold tracking-wide uppercase">
             {title}
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            A better way to send money
+            Tools I Have Been Working With
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-            voluptatum cupiditate veritatis in accusamus quisquam.
+            The combination of technologies I have used in a team setting to build and run production-level applications.
           </p>
         </div>
 
@@ -21,7 +20,7 @@ function TechStack({ title }) {
           <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
             <div className="relative">
               <dt>
-                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-stone-500 text-white">
                   <svg
                     className="h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -51,7 +50,7 @@ function TechStack({ title }) {
 
             <div className="relative">
               <dt>
-                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-stone-500 text-white">
                   <svg
                     className="h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -81,7 +80,7 @@ function TechStack({ title }) {
 
             <div className="relative">
               <dt>
-                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-stone-500 text-white">
                   <svg
                     className="h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -111,7 +110,7 @@ function TechStack({ title }) {
 
             <div className="relative">
               <dt>
-                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-stone-500 text-white">
                   <svg
                     className="h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
